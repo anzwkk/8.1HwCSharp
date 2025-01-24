cfc9e90d271c862451dfe6ef025550c577b27820
anzwkk <anam89737@gmail.com>
2025-01-24 18:44:47 +0300

    output the element of the array, (\t) for tabulation

--- HW8.1/Program.cs
+++ HW8.1/Program.cs
@@ -51,7 +51,7 @@ for (int i = 0; i < 9; i++)
     for (int j = 0; j < 9; j++) 
     {
         multipTable[i, j] = (i + 1) * (j + 1);
-
+        Console.Write(multipTable[i, j] + "\t");
     }
     Console.WriteLine();
 }
