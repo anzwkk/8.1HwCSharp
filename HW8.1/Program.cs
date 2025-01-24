e9cedc50ad862b46401362aa1e018714675c41de
anzwkk <anam89737@gmail.com>
2025-01-24 19:11:03 +0300

    Filling the array with random numbers

--- HW8.1/Program.cs
+++ HW8.1/Program.cs
@@ -44,14 +44,55 @@
 //}
 
 // Створити та заповнити двовимірний масив розміру 9х9 з результатами таблиці множення (у першому рядку мають бути записані добутки кожного з чисел від 1 до 9 на 1, у другому – на 2, ..., в останньому – на 9). Тобто в 1 строчці будуть значення від 1 до 9, а у другому 2,4,6,....,18 і т.д.
-int[,] multipTable = new int[9, 9];
+//int[,] multipTable = new int[9, 9];
 
-for (int i = 0; i < 9; i++) 
+//for (int i = 0; i < 9; i++) 
+//{
+//    for (int j = 0; j < 9; j++) 
+//    {
+//        multipTable[i, j] = (i + 1) * (j + 1);
+//        Console.Write(multipTable[i, j] + "\t");
+//    }
+//    Console.WriteLine();
+//}
+
+//4. Створити двовимірний масив 5х5. Заповнити його будь-якими числами. Визначити та вивести на екран: а) максимальний елемент масиву; б) мінімальний елемент масиву; в) координати мінімального елемента масиву. г) координати максимального елемента масиву.
+int[,] array = new int[5, 5];
+Random rand =  new Random();
+
+for (int i = 0; i < 5; i++)
 {
-    for (int j = 0; j < 9; j++) 
+    for (int j = 0; j < 5; j++)
     {
-        multipTable[i, j] = (i + 1) * (j + 1);
-        Console.Write(multipTable[i, j] + "\t");
+        array[i, j] = rand.Next(-50, 50);
+        Console.Write(array[i, j] + "\t");
     }
     Console.WriteLine();
 }
+int max = array[0, 0];
+int min = array[0, 0];
+int maxRow =  array[0, 0];
+int minRow = array[0, 0];
+int maxCol =  array[0, 0];
+int minCol = array[0, 0];
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        if (array[i, j] > max)
+        {
+            max = array[i, j];
+            maxRow = i;
+            maxCol = j;
+        }
+        if (array[i, j] < min)
+        {
+            min = array[i, j];
+            minRow = i;
+            minCol = j;
+        }
+    }
+}
+Console.WriteLine("Максимальний елемент: " + max + " на позиції (" + maxRow + ", " + maxCol + ")");
+Console.WriteLine("Мінімальний елемент: " + min + " на позиції (" + minRow + ", " + minCol + ")");
