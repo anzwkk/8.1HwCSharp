44e78351712ee611197bbf68d797b25b79095bbd
anzwkk <anam89737@gmail.com>
2025-01-24 17:37:17 +0300

    Fill the array with random values

--- HW8.1/Program.cs
+++ HW8.1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. Створити масив із 10 елементів типу int. Присвоїти їм випадкові значення від -10 до 10, використовуючи клас Random (https://learn.microsoft.com/en-us/dotnet/api/system.random?view=net-8.0) Вивести на екран елементи масиву з парним індексом. (не парні значення, а саме парні індекси!!)
+int[] array = new int[10];
+Random rand = new Random();
+
+for (int i = 1; i < array.Length; i++)
+{
+    array[i] = rand.Next(-10, 11);
+}
